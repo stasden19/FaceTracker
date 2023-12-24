f7ea92e4fceaec46ad8bc5273cbf8d3e4ec4e7b5
stasden <stasden17@gmail.com>
2023-12-24 17:15:05 +0300

    Проект доделан
    Осталось получше обучить нс и будет конфетка, возможно добавить цвета

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
-import ultralytics
 import cv2
 import numpy as np
-import cvzone
 
-model = ultralytics.YOLO('./runs/detect/train3/weights/best.pt')
-
-# Define current screenshot as source
-source = '1.jpg'
-frame = cv2.imread(source)
-# Run inference on the source
-results = model(frame, classes=[1])
-for result in results:
-        boxes = result.boxes.cpu().numpy()
-        for box in boxes:
-            x1, y1, x2, y2 = box.xyxy[0].astype(int)
-            w, h = x2 - x1, y2 - y1
-
-            cvzone.cornerRect(frame, (x1, y1, w, h), l=5, t=5)
-
-            # currentArray = np.array((x1, y1, x2, y2))
-            # detections = np.vstack((detections, currentArray))
-cv2.imshow('Look', frame)
-cv2.waitKey(0)
+from yolo_segmentation import YOLOSegmentation
+video = cv2.VideoCapture('video_2023-12-24_13-34-47.mp4')
+ys = YOLOSegmentation(r"J:\AllProjectsPython\FaceTracker\runs\segment\train5\weights\best.pt")
+while video.isOpened():
+    ret, img = video.read()
+    img = cv2.resize(img, (640, 640))
+    bboxes, classes, segmentations, scores = ys.detect(img)
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_gray = cv2.cvtColor(img_gray, cv2.COLOR_GRAY2BGR)
+    img_gray = cv2.medianBlur(img_gray, 31)
+    all_res = list(zip(bboxes, classes, segmentations, scores))
+    for bbox, class_id, seg, score in zip(bboxes, classes, segmentations, scores):
+        (x, y, x2, y2) = bbox
+        mask = np.zeros_like(img_gray)
+        cv2.fillPoly(mask, [seg], color=[255, 255, 255])
+    gray_mask = cv2.bitwise_and(img_gray, mask)
+    combined_image = np.where(mask == 255, img_gray, img)
+    cv2.imshow("Image", combined_image)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cv2.destroyAllWindows()
